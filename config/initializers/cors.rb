1dd690fd75c659f93f564361d57df70648d95655
yahyaelganyni1 <yahyaelganayni@gmail.com>
2023-12-01 22:21:25 +0200

    Update CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,10 +6,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/packs/*', headers: :any, methods: [:get, :options]
-    resource '/audio/*', headers: :any, methods: [:get, :options]
-    # Make the public endpoints accessible to the frontend
-    resource '/public/api/*', headers: :any, methods: :any
+    # resource '/packs/*', headers: :any, methods: [:get, :options]
+    # resource '/audio/*', headers: :any, methods: [:get, :options]
+    # # Make the public endpoints accessible to the frontend
+    # resource '/public/api/*', headers: :any, methods: :any
+    #
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
 
     if ActiveModel::Type::Boolean.new.cast(ENV.fetch('CW_API_ONLY_SERVER', false)) || Rails.env.development?
       resource '*', headers: :any, methods: :any, expose: %w[access-token client uid expiry]
